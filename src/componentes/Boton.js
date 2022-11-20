b325d8b232882b0df6f2bf640cbb21fd9f5ee6e4
YBucca <yani.bucca@gmail.com>
2022-11-19 21:08:02 -0300

    contador ok

---
+++ src/componentes/Boton.js
@@ -0,0 +1,9 @@
+import React from "react";
+import '../style/Boton.css'
+
+function Boton( {texto, esBotonDeClick, manejarClick}){
+  return(
+    <button className={esBotonDeClick ? "btn-click" : "btn-reiniciar"} onClick={manejarClick}>{texto}</button>
+  )
+}
+export default Boton;
